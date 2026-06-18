e7d970a1b7c269e7eafcb55c773677a681224e4c
Calvin Visscher <visschc@hva.nl>
2026-06-18 16:33:35 +0200

    Add: Main Page + Student Wrapped working

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import "dotenv/config";
 import { readFile } from "node:fs/promises";
 
 const app = express();
@@ -7,15 +8,15 @@ const app = express();
 app.use(express.static("static"));
 
 app.get("/", async (req, res) => {
-    const data = await readFile("./server.html", "utf8");
+    const data = await readFile("./static/html/index.html", "utf8");
     res.send(data);
 });
 
 app.get("/student-wrapped", async (req, res) => {
-    const data = await readFile("./student wrapped/index.html", "utf8");
+    const data = await readFile("./static/html/student-wrapped.html", "utf8");
     res.send(data);
 });
 
 app.listen(process.env.PORT, "0.0.0.0", () => {
-    console.log(`Server running on port: ${CONFIG.PORT}`);
+    console.log(`Server running on port: ${process.env.PORT}`);
 });
